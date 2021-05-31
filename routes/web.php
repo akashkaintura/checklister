81fb5dbb82d380d448016c2e120f8947cd89f088
Akash <cole.akash@Akash.local>
2021-05-31 21:46:24 +0530

    feature: Manage Checklists in sidebar

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\ChecklistController;
+use App\Http\Controllers\Admin\ChecklistGroupController;
 use App\Http\Controllers\Admin\PageController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -25,6 +27,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::group(['middleware' => 'auth'], function () {
     Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'is_admin'], function () {
-        Route::resource('pages', \App\Http\Controllers\Admin\PageController::class);
+        Route::resource('pages', PageController::class);
+        Route::resource('checklist_groups', ChecklistGroupController::class);
+        Route::resource('checklists', ChecklistController::class);
     });
 });
